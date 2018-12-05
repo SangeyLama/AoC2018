a77863d7e3e45676b0b2bb2b15e73fb1758b9969
Sangey Dorje Leon Lama <sll@neasenergy.com>
2018-12-05 12:47:58 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day01.FrequencyProblem());
+            Console.WriteLine(Day01.RepeatedFrequencyProblem());
+            Console.ReadLine();
+        }
+
+
+    }
+}
